764dc025907c9125ae519bbf5f0af68fc33c330c
peehscavacini <30813668+peehscavacini@users.noreply.github.com>
2017-09-12 22:08:35 -0300

    Update jogovelha.py

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -3,7 +3,7 @@ def inicializar():
     for i in range(3):
         linha = []
         for j in range(3):
-            linha.append(".")
+            linha.append("xpamonha")
         tab.append(linha)
     return tab
 
