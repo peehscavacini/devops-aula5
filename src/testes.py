7a2131ada100452380b30db9cb7fb1a664c50d65
peehscavacini <30813668+peehscavacini@users.noreply.github.com>
2017-09-12 21:33:00 -0300

    Update testes.py

--- src/testes.py
+++ src/testes.py
@@ -1,20 +1,20 @@
-Python 3.5.4 (v3.5.4:3f56838, Aug  8 2017, 02:07:06) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> Import jogovelha
+import jogovelha
 import sys
+
 erroInicializar = False
 jogo = jogovelha.inicializar()
-if len(jogo) != 3:
-erroInicializar = True
+
+if len(jogo) !=3:
+    erroInicializar = True
 else:
-for linha in jogo:
-if len(linha) != 3:
-erroInicializar = True
-else:
-for elemento in linha:
-if elemento != '.':
-erroInicializar = True
+    for linha in jogo:
+        if len(linha) != 3:
+            erroInicializar = True
+        else:
+            for elemento in linha:
+                if elemento != '.':
+                    erroInicializar = True
 if erroInicializar:
-sys.exit(1)
+    sys.exit(1)
 else:
-sys.exit(0)
+    sys.exit(0)
